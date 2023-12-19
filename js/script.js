282bfa1dcb5cbf4c09f480d3434a37d67842e96e
FrancescoM95 <mon95fran@gmail.com>
2023-12-19 16:44:36 +0100

    scaffolding

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
 console.log('JS OK');
+
+
+//* INITIAL VARIABLES
+
+const kms = document.getElementById('kms');
+const age = document.getElementById('age');
+const button = document.getElementById('button');
+const total = document.getElementById('total');
+const priceMessage = 'Il totale del tuo biglietto è: €';
+const pricePerKm = 0.21;
+let discount = null;
+
+
+//* MAIN LOGICS
+
+button.addEventListener('click', function () {
+    const distance = kms.value;
+});
+
